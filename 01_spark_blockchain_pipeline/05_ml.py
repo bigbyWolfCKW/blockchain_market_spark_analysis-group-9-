a8a7188d94490a7009f68517a1997c37e002d871
kwfanaa <kwfanaa@connect.ust.hk>
2026-05-03 13:41:28 +0800

    add script

---
+++ 01_spark_blockchain_pipeline/05_ml.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
+from loguru import logger
+from pathlib import Path
+
+DATA_FOLDER = Path("./data")
+OUTPUT_FOLDER = Path("./output")
+
+def main():
+    logger.info("=== Building Daily Features and BTCUSDT data ===")
+    spark = (
+        SparkSession.builder
+        .appName("load-aws-btc-transactions")
+        .config("spark.driver.memory", "8g")
+        .getOrCreate()
+    )
+    logger.info(f"=== Reading Daily Features from {OUTPUT_FOLDER} ===")
+    df_features = spark.read.option("basePath", str(OUTPUT_FOLDER)).parquet(str(Path(OUTPUT_FOLDER, "daily_features")))
+
+    logger.info(f"=== Reading BTCUSDT from {OUTPUT_FOLDER} ===")
+    df_btcusdt = spark.read.option("basePath", str(OUTPUT_FOLDER)).parquet(str(Path(OUTPUT_FOLDER, "BTCUSDT")))
+
+    final_df = df_features.join(df_btcusdt, on="date", how="left")
+    final_df.head(5)
+    spark.stop()
+    return
+
+if __name__ == "__main__":
+    main()
